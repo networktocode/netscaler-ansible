6aa36ba6db18269bf0512cfe6b8a23b22ddc0011
Jacob McGill <jacob@networktocode.com>
2017-07-24 13:14:23 -0400

    fix url for non-standard ports

--- library/netscaler_server.py
+++ library/netscaler_server.py
@@ -200,14 +200,17 @@ class Netscaler(object):
         self.verify = verify
         self.api_endpoint = api_endpoint
         self.headers = {"Content-Type": "application/json"}
-        self.port = kwargs.get("port", "")
+        if "port" not in kwargs:
+            self.port = ""
+        else:
+            self.port = ":{}".format(kwargs["port"])
 
         if use_ssl:
-            self.url = "https:{port}//{lb}/nitro/v1/config/".format(port=self.port, lb=self.host)
-            self.stat_url = "https:{port}//{lb}/nitro/v1/stat/".format(port=self.port, lb=self.host)
+            self.url = "https://{lb}{port}/nitro/v1/config/".format(lb=self.host, port=self.port)
+            self.stat_url = "https://{lb}{port}/nitro/v1/stat/".format(lb=self.host, port=self.port)
         else:
-            self.url = "http:{port}//{lb}/nitro/v1/config/".format(port=self.port, lb=self.host)
-            self.stat_url = "http:{port}//{lb}/nitro/v1/stat/".format(port=self.port, lb=self.host)
+            self.url = "http://{lb}{port}/nitro/v1/config/".format(lb=self.host, port=self.port)
+            self.stat_url = "http://{lb}{port}/nitro/v1/stat/".format(lb=self.host, port=self.port)
 
     def change_state(self, object_name, state):
         """
@@ -766,7 +769,7 @@ def change_config(session, module, proposed, existing):
 
     elif config_method == "update":
         if "ipaddress" in config_diff:
-            module.fail_json(msg="Updating IP Addresses is not Supported")
+            module.fail_json(msg="Updating IP Addresses is not Supported.")
 
         if "td" in config_diff:
             module.fail_json(msg="Updating a Server's Traffic Domain is not Supported")
