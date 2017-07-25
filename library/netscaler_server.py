1cbb0b5ea8c4815e675331ad6fae616b0f05a137
Jacob McGill <jacob@networktocode.com>
2017-07-25 17:03:15 -0400

    move rename methods to base class

--- library/netscaler_server.py
+++ library/netscaler_server.py
@@ -217,6 +217,21 @@ class Netscaler(object):
             self.url = "http://{lb}{port}/nitro/v1/config/".format(lb=self.host, port=self.port)
             self.stat_url = "http://{lb}{port}/nitro/v1/stat/".format(lb=self.host, port=self.port)
 
+    def change_name(self, existing_name, proposed_name):
+        """
+        The purpose of this method is to change the name of a server object.
+        :param existing_name: Type str.
+                              The name of the server object to be renamed.
+        :param proposed_name: Type str.
+                              The new name of the server object.
+        :return: The response from the request to delete the object.
+        """
+        url = self.url + self.api_endpoint + "?action=rename"
+        body = {self.api_endpoint: {"name": existing_name, "newname":proposed_name}}
+        response = self.session.post(url, json=body, headers=self.headers, verify=self.verify)
+
+        return response
+
     def change_state(self, object_name, state):
         """
         The purpose of this method is to change the state of an object from either disabled to enabled, or enabled to
@@ -277,6 +292,33 @@ class Netscaler(object):
                 module.fail_json(msg=config_status.content)
         else:
             config.append({"method": "post", "url": self.url + self.api_endpoint, "body": new_config})
+
+        return config
+
+    def config_rename(self, module, existing_name, proposed_name):
+        """
+        This method is used to handle the logic for Ansible modules when the "state" is set to "present" and the
+        proposed IP Address matches the IP Address of another Server in the same Traffic Domain. The change_name
+        method is used to post the configuration to the Netscaler.
+        :param module: The AnsibleModule instance started by the task.
+        :param existing_name: Type str.
+                              The current name of the Server object to be changed.
+        :param proposed_name: Type str.
+                              The name the Server object should be changed to.
+        :return: A list with config dictionary corresponding to the config returned by the Ansible module.
+        """
+        config = []
+
+        rename_config = {"name": existing_name, "newname": proposed_name}
+
+        if not module.check_mode:
+            config_status = self.change_name(existing_name, proposed_name)
+            if config_status.ok:
+                config.append({"method": "post", "url": config_status.url, "body": rename_config})
+            else:
+                module.fail_json(msg=config_status.content)
+        else:
+            config.append({"method": "post", "url": self.url + self.api_endpoint + "?action=rename", "body": rename_config})
 
         return config
 
@@ -595,21 +637,6 @@ class Server(Netscaler):
     def __init__(self, host, user, passw, secure=True, verify=False, api_endpoint="server", **kwargs):
         super(Server, self).__init__(host, user, passw, secure, verify, api_endpoint, **kwargs)
 
-    def change_name(self, existing_name, proposed_name, module):
-        """
-        The purpose of this method is to change the name of a server object.
-        :param existing_name: Type str.
-                              The name of the server object to be renamed.
-        :param proposed_name: Type str.
-                              The new name of the server object.
-        :return: The response from the request to delete the object.
-        """
-        url = self.url + self.api_endpoint + "?action=rename"
-        body = {self.api_endpoint: {"name": existing_name, "newname":proposed_name}}
-        response = self.session.post(url, json=body, headers=self.headers, verify=self.verify)
-
-        return response
-
     def check_duplicate_ip(self, proposed):
         """
         The purpose of this method is to identify if the proposed server has an "ipaddress" value that already exists in
@@ -632,31 +659,6 @@ class Server(Netscaler):
             colliding_ip_dict["existing_name"] = colliding_ip["name"]
 
         return colliding_ip_dict
-
-    def config_rename(self, module, existing_name):
-        """
-        This method is used to handle the logic for Ansible modules when the "state" is set to "present" and the
-        proposed IP Address matches the IP Address of another Server in the same Traffic Domain. The change_name
-        method is used to post the configuration to the Netscaler.
-        :param module: The AnsibleModule instance started by the task.
-        :param existing_name: Type str.
-                              The current name of the Server object to be changed.
-        :return: A list with config dictionary corresponding to the config returned by the Ansible module.
-        """
-        config = []
-
-        rename_config = {"name": existing_name, "newname": module.params["server_name"]}
-
-        if not module.check_mode:
-            config_status = self.change_name(existing_name, module.params["server_name"], module)
-            if config_status.ok:
-                config.append({"method": "post", "url": config_status.url, "body": rename_config})
-            else:
-                module.fail_json(msg=config_status.content)
-        else:
-            config.append({"method": "post", "url": self.url + self.api_endpoint + "?action=rename", "body": rename_config})
-
-        return config
 
     def get_all(self):
         """
@@ -810,7 +812,7 @@ def change_config(session, module, proposed, existing):
             module.fail_json(msg="Changing a Server's Name requires setting the config_override param to True:\n{}".format(dup_ip_check))
         elif dup_ip_check:
             changed = True
-            rename = session.config_rename(module, dup_ip_check["existing_name"])
+            rename = session.config_rename(module, dup_ip_check["existing_name"], proposed["name"])
             new_existing = session.get_existing_attrs(proposed["name"], proposed.keys())
             config_method, config_diff = Server.get_diff(proposed, new_existing)
 
